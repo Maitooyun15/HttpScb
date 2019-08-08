fd4638ababd0a857c5d55cd134b0983be1e049ee
Siripron Thammarakkitanon <>
2019-08-08 21:04:30 +0700

    edit all

---
+++ Pods/Target Support Files/Pods-HttpScb/Pods-HttpScb-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HttpScb : NSObject
+@end
+@implementation PodsDummy_Pods_HttpScb
+@end
---
+++ Pods/Target Support Files/Pods-HttpScbTests/Pods-HttpScbTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HttpScbTests : NSObject
+@end
+@implementation PodsDummy_Pods_HttpScbTests
+@end
