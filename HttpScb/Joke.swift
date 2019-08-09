72a812958ae47bf7057caa7e39901c3b7f12665d
Siripron Thammarakkitanon <>
2019-08-09 10:39:43 +0700

    edit

--- HttpScb/Joke.swift
+++ HttpScb/Joke.swift
@@ -8,8 +8,16 @@
 
 import Foundation
 
-struct Joke {
+struct Joke: Codable { 
     let id: String
     let value: String
     let iconURL: String
+    let url: String
+}
+
+private enum CodingKeys : String  , CodingKey {
+    case id
+    case value
+    case url
+    case iconURL = "icon_url"
 }
--- HttpScb/ViewController.swift
+++ HttpScb/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
 
     @IBAction func buttonTapped(_ sender: Any) {
         APIManager().getRandomJokeAlamofire()
+       // APIManager.getRandomJoke()
     }
 }
 
